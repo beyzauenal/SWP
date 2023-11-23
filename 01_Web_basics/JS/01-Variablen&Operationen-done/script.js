2ed30fc66f5551eed5c4b0030b12a995305a2638
beyzauenal <beyza.uenal@student.htldornbirn.at>
2023-11-23 09:30:40 +0100

    new task

---
+++ 01_Web_basics/JS/01-Variablen&Operationen-done/script.js
@@ -0,0 +1,20 @@
+console,console.log("Hallo");
+console,console.log("Tolle sache");
+
+//Variablen
+let a = 7;
+let b = 8;
+let c = a+b;
+
+console.log(" A hat den Wert " + a);
+
+
+console.log(" A hat den Wert " + a);
+
+console.log("a+b= " +c);
+
+//Rechnungsarten
+// *, /, +, -
+
+c= b-a;
+console.log("b-a= " +c);
