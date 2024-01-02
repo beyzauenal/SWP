80dd97144ceec075a96a137d258f0117dd4a2105
beyzauenal <beyza.uenal@student.htldornbirn.at>
2024-01-02 21:16:42 +0100

    05 If-Bedingung_done

--- 01_Web_basics/JS/05 If-Bedingung_done/script2.js
+++ 01_Web_basics/JS/05 If-Bedingung_done/script2.js
@@ -9,3 +9,55 @@ if (number<25){
     console.log("else");
 }
 
+
+//Aufgabe 1
+
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini" 
+let number = 15;
+
+if(number<20){
+    console.log("mini");
+}
+
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium" 
+let number = 30;
+
+if(number<20){
+    console.log("mini");
+} else if (number >=20 && number <=50) {
+    console.log("medium");
+}
+
+// Wenn die Zahl größer als 50 ist gib aus "Large" 
+let number = 55;
+if(number>50){
+    console.log("large");
+}
+ 
+
+ 
+
+// Aufgabe 2 
+
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini" 
+let number = 6;
+if(number<7){
+    console.log("mini");
+}
+
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium" 
+let number = 50;
+if(number <7){
+    console.log("mini");
+}else if (number >=7 && number<=90){
+    console.log(medium);
+}
+
+// Wenn die Zahl größer als 90 ist gib aus "Large" 
+let number = 100;
+if(number>90){
+    console.log("large");
+}
+ 
+
+ 
