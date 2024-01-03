08dba235b6e2c1b9d93fe1d72430d3f8a8df5a59
beyzauenal <beyza.uenal@student.htldornbirn.at>
2024-01-03 17:11:23 +0100

    06 Zufallszahlen erstellen_process

--- 01_Web_basics/JS/06 Zufallszahlen erstellen_process/script.js
+++ 01_Web_basics/JS/06 Zufallszahlen erstellen_process/script.js
@@ -1,4 +1,7 @@
-let newNumber = Math.random();
+let randomNumber = Math.random() *40;
+let random = Math.round(randomNumber);
 
-console.log(randomNumber);
-sdmfks
+console.log(random);
+
+
+//1 2 3 4 5 6 7 8 9 10 11 ....
