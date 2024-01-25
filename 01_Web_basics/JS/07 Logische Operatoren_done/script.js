110ed89a2378e9c22184d0f6a6fb5fa35c5b5e22
beyzauenal <beyza.uenal@student.htldornbirn.at>
2024-01-25 09:35:16 +0100

    07 Logische Operatoren_done

---
+++ 01_Web_basics/JS/07 Logische Operatoren_done/script.js
@@ -0,0 +1,38 @@
+//logische Operatoren
+if ((4>7)&&(3<4)){
+    console.log("Is true");
+}
+
+// Erstelle zwei Zufallszahl zwischen 0 und 100
+// Speichere diese jeweils in einer Variable
+if ((7<8)&&(77>88)){
+    console.log("Is true");
+}
+
+// Wenn die erste Zahl kleiner ist als die zweite UND die erste Zahl kleiner ist als 50
+// dann gib aus "Zahl 1 ist kleiner als Zahl 2 und Mini"
+let Zahl1= 40;
+let Zahl2= 45;
+
+if((40<49)&&(40<50)){
+    console.log("Zahl 1 ist kleiner als Zahl 2 und Mini");
+}
+
+
+// Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30
+// dann gib aus "Eine der beiden ist kleiner als 30"
+let zahl1 = 25;
+let zahl2 = 40;
+
+if((25<30) || (40 < 30)) {
+  console.log("Eine der beiden ist kleiner als 30");
+}
+
+// Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist
+// dann gib aus "Erste Zahl klein, zweite kein 50iger"
+let zahl1 = 40;
+let zahl2 = 60;
+
+if (zahl1 < 50 && zahl2 !== 50) {
+  console.log("Erste Zahl klein, zweite kein 50iger");
+}
