76638505f681457c68930d91c44d30c5f8639e19
beyzauenal <beyza.uenal@student.htldornbirn.at>
2024-05-16 09:32:56 +0200

    11 Arrays_endoflesson

---
+++ 01_Web_basics/JS/11 Arrays_endoflesson/11-arrays.js
@@ -0,0 +1,28 @@
+let a = [];
+let b = [4, 3, 2];
+
+let arr = [4, 9, 8];
+console.log(arr[1]);
+
+arr.push(10);
+arr.push(3);
+
+for (let index = 0; index < arr.length; index++) {
+  console.log(arr[index]);
+}
+
+// //Aufgabe 1)
+// array = [4, 1, 2, 3];
+
+// array.push(17);
+// array.push(199);
+
+// console.log("Alle Werte im Array:");
+// for (let i = 0; i < array.lenght; i++) {
+//   console.log(arr(index));
+// }
+
+// console.log("Summe aller Werte:", summe);
+
+// let mitttelwert = summe / array.lenght;
+// console.log("Der Mittelwert der Zahlen:", mittelwert);
